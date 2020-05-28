e0a53f29f95dbae2859fcb134a866ccaf111e0e8
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-28 23:08:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,3 @@
-class Artist
-  attr_accessor :name, :title
-  def initialize(name, title)
-    @name = name
-    @title = title
-  end
-end
-
-lanadelrey = Artist.new("Lana Del Rey", "Summertime Sadness")
-summertime_sadness = Song.new("Summertime Sadness")
-summertime_sadness.artist = lanadelrey
-summertime_sadness.artist.title #=> Summertime Sadness
-summertime_sadness.artist.name #=> Lana Del Rey
+class Song
+  attr_accessor :title, :artist
+end 
